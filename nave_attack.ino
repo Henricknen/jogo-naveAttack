97051af6c600c7a7cbc6d59f3d48fd30de24fa1d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-01-05 13:37:56 -0300

    continuando_desenvovimento_do_jogo_criando_diversas_funcoes_e_definindo_'caracteres'_dos_elementos_de_'nave_tiro_asteroides_explosao_tiro'

--- nave_attack.ino
+++ nave_attack.ino
@@ -1,39 +1,95 @@
-#include <LiquidCrystal.h>		// fazendo include da bibioteca 'LiquidCrystal'
+#include <LiquidCrystal.h>		// fazendo incue da biblioteca 'LiquidCrystal'
 
-#define btcima 8		// 'definição' dos botões
+#define btcima 8		// definições
 #define btbaixo 9
 #define bttiro 10
 
-LiquidCrystal.lcd(7, 6, 5, 4, 3, 2);	// objeto do tipo 'LiquidCrystal'
+LiquidCrystal lcd(7,6,5,4,3,2);
 
-int vel = 100;		// variável de veloçidade
-int pxnave, pynave, pxaste, pyaste, pxenergia, pyenergia, pxtiro, pytiro;		// variável de posição dos elementos do jogo
-bool game, vtiro, vpilha;		// variáveis boleana que recebe 'true', 'false' ou '0' e '1'
+int vel = 100;
+int pxnave, pynave, pxaste, pyaste, pxenergia, pyenergia, pxtiro, pytiro;
+bool game, vtiro, vpilha;
 int pontos;
 double venergia;
 
-byte nave[8]={ B11000, B01100, B01110, B01111, B01111, B01110, B01100, B11000};
-byte asteroide[8]={ B00000, B00100, B01110, B10111, B11101, B01110, B00100, B00000};
-byte explosao[8]={B10001,B10101,B01010,B10100,B00101,B01010,B10101,B10001};
-byte energia[8]={B01110,B11011,B10001,B10101,B10101,B10101,B10001,B11111};
-byte tiro[8]={B00000,B00000,B11110,B01111,B11110,B00000,B00000,B00000};
+byte nave[8] = {B11000,B01100,B01110,B01111,B01111,B01110,B01100,B11000};
+byte asteroide[8] = {B00000,B00100,B01110,B10111,B11101,B01110,B00100,B00000};
+byte explosao[8] = {B10001,B10101,B01010,B10100,B00101,B01010,B10101,B10001};
+byte energia[8] = {B01110,B11011,B10001,B10101,B10101,B10101,B10001,B11111};
+byte tiro[8] = {B00000,B00000,B00000,B00111,B00111,B00000,B00000,B00000};
 
-void setup(){
-  pxnave = pynave = pontos = pyaste = pxenergia = pyenergia  = pytiro = 0;
-  pxtiro = -1;		// '-1' indica que o tiro está la mas so que estará fora da tela
-  pxaste = 12;			// o asteroide iniçiará na posição 12
-  energia = 100;			// iniçiará cheia
-  
-  lcd.createChar(1, nave);		// criação do caracteres do jogo
+void setup() {
+  pxnave = pynave = pyaste = pontos = pxenergia = pyenergia = pytiro = 0;		// fazendo a iniçiaização dos elementos
+  pxtiro = -1;
+  pxaste = 12;
+  venergia = 100;
+  lcd.createChar(1, nave);		// criando caracters dos elementos (nvae, tiro, asteroide etc)
   lcd.createChar(2, asteroide);
   lcd.createChar(3, explosao);
   lcd.createChar(4, energia);
   lcd.createChar(5, tiro);
-  lcd.begin(16, 2);		// iniçializando o tamaho do display
+  lcd.begin(16, 2);
   lcd.clear();
-  game = true;		// 'true' indica que o game está em funcionamento
-  vtiro = vpilha = false;		// inidia que o jogo iniçiara sem tiro ou pilha na tela
+  game = true;
+  vtiro = vpilha = false;
 }
 
 void loop(){
-  }
+  
+}
+
+void desenhaNave(int px, int py){
+  lcd.setCursor(px, py);
+  lcd.write(1); 
+}
+
+void desenhaAsteroide(int px, int py){
+  lcd.setCursor(px, py);
+  lcd.write(2);
+}
+void desenhaEnergia(int px, int py){
+  lcd.setCursor(px, py);
+  lcd.write(4);
+}
+void desenhaTiro(int px, int py){
+  lcd.setCursor(px, py);
+  lcd.write(5);
+}
+void desenhaExplosaoNave(int px, int py){
+  lcd.clear();
+  lcd.setCursor(px, py);
+  lcd.write(3);
+  delay(1000);
+  lcd.clear();
+}
+void desenhaExplosaoAsteroide(int px, int py){
+  lcd.setCursor(px, py);
+  lcd.write(3);
+  delay(1000);
+  lcd.clear();
+}
+void reset(){
+  pontos = 0;
+  venergia = 100;
+  game=true;
+}
+void painel(int px){
+  lcd.setCursor(px, 0);
+  lcd.print(pontos);
+  lcd.setCursor(px, 1);
+  lcd.print(venergia);
+}
+void tela(int cond){// função de 'vitoria' ou 'derrota'
+  char txt[6] = {(cond > 0 ? "GANHOU" : "PERDEU")};
+  lcd.setCursor(9, 0);
+  lcd.print("pts:");
+  lcd.setCursor(13, 0);
+  lcd.print(pontos);
+  lcd.setCursor(1, 0);
+  lcd.print(txt);
+  lcd.setCursor(0, 1);
+  lcd.print("Pressione tiro");
+}
+
+
+
